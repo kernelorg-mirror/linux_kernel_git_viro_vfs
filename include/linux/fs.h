6805bd4731eb1b0336bd37996971e0ac0f9f08ca
Al Viro <viro@zeniv.linux.org.uk>
2025-11-01 01:54:52 -0400

    get rid of audit_reusename()
    Originally we tried to avoid multiple insertions into audit names array
    during retry loop by a cute hack - memorize the userland pointer and
    if there already is a match, just grab an extra reference to it.
    
    Cute as it had been, it had problems - two identical pointers had
    audit aux entries merged, two identical strings did not.  Having
    different behaviour for syscalls that differ only by addresses of
    otherwise identical string arguments is obviously wrong - if nothing
    else, compiler can decide to merge identical string literals.
    
    Besides, this hack does nothing for non-audited processes - they get
    a fresh copy for retry.  It's not time-critical, but having behaviour
    subtly differ that way is bogus.
    
    These days we have very few places that import filename more than once
    (9 functions total) and it's easy to massage them so we get rid of all
    re-imports.  With that done, we don't need audit_reusename() anymore.
    There's no need to memorize userland pointer either.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2835,7 +2835,6 @@ extern struct kobject *fs_kobj;
 struct audit_names;
 struct filename {
 	const char		*name;	/* pointer to actual string */
-	const __user char	*uptr;	/* original userland pointer */
 	atomic_t		refcnt;
 	struct audit_names	*aname;
 	const char		iname[];
