85863f5773c15f02ff6518ad034e3b380b78ca53
Al Viro <viro@zeniv.linux.org.uk>
2026-05-12 12:53:35 -0400

    kill configfs_drop_dentry()
    Fold into the only remaining user, don't bother with the timestamps
    of parent - we are going to rmdir it shortly anyway, which will
    override those.
    
    Fix the locking of inode, while we are at it - updating the link
    count and timestamps ought to be done with the inode locked.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/configfs/dir.c
+++ fs/configfs/dir.c
@@ -562,12 +562,33 @@ static void detach_attrs(struct dentry *dentry)
 
 	parent_sd = dentry->d_fsdata;
 	list_for_each_entry_safe(sd, tmp, &parent_sd->s_children, s_sibling) {
+		struct dentry *child;
 		if (!sd->s_element || !(sd->s_type & CONFIGFS_NOT_PINNED))
 			continue;
 		spin_lock(&configfs_dirent_lock);
 		list_del_init(&sd->s_sibling);
+		child = sd->s_dentry;
+		if (child) {
+			spin_lock(&child->d_lock);
+			if (simple_positive(child)) {
+				dget_dlock(child);
+				spin_unlock(&child->d_lock);
+			} else {
+				spin_unlock(&child->d_lock);
+				child = NULL;
+			}
+		}
 		spin_unlock(&configfs_dirent_lock);
-		configfs_drop_dentry(sd, dentry);
+		if (child) {
+			struct inode *inode = child->d_inode;
+			d_drop(child);
+
+			inode_lock_nested(inode, I_MUTEX_NONDIR2);
+			inode_set_ctime_current(inode);
+			drop_nlink(inode);
+			inode_unlock(inode);
+			dput(child);
+		}
 		configfs_put(sd);
 	}
 }
